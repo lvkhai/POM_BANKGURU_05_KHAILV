3b9c278a6a93bd77b7d035e5bdac95c97ed228ac
khaile <khaile@172.24.29.110>
2018-09-20 21:24:38 +0700

    Edit testcase User01

--- testcases/com/bankguru/user/User01_RegisterAndLogin.java
+++ testcases/com/bankguru/user/User01_RegisterAndLogin.java
@@ -1,8 +1,16 @@
 package com.bankguru.user;
 
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
 public class User01_RegisterAndLogin {
+@BeforeClass
+public void beforeClass() {}
 @Test
 public void demo() {}
+
+@AfterClass
+public void afterClass() {	
+}
 }
