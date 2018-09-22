d94a54ef0780a2bffb0b3917c31473ff88b7a0f8
khaile <khaile@192.168.100.2>
2018-09-22 23:19:40 +0700

    User Interactions

--- actions/commons/AbstractPage.java
+++ actions/commons/AbstractPage.java
@@ -9,6 +9,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.Select;
 import org.openqa.selenium.support.ui.WebDriverWait;
@@ -182,5 +183,16 @@ public class AbstractPage {
 	public void switchToIframe(WebDriver driver, String locator) {
 		driver.switchTo().frame(locator);
 	}
+	
+	//User Actions
+	
+	public void doubleClickToElement(WebDriver driver, String locator) {
+		WebElement element = driver.findElement(By.xpath(locator));
+		Actions actions = new Actions(driver);
+		actions.doubleClick(element);
+		
+
+	}
+	
 
 }
